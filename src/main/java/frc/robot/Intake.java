41f5d6be95e8899e3e2a685a62d71a8b6f2ca97b
aahana <86615215+aahana007@users.noreply.github.com>
2022-04-12 11:42:14 -0500

    Intake Skeleton

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -1,5 +1,26 @@
 package frc.robot;
 
-public class Intake {
-    // one motor
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Intake extends TimedRobot {
+
+    public static final double DEFAULT_SPEED = 1.0;
+    public static final boolean REVERSE = false;
+    public static final NeutralMode NEUTRALMODE = NeutralMode.Coast;
+    public static final double RAMPING_FROM_0_TO_FULL = 0.0; // 0.5
+
+    private CANSparkMax intakeMotor;
+
+    public Intake(CANSparkMax motor) {
+        intakeMotor = motor;
+        intakeMotor.setInverted(REVERSE);
+
+    }
+
+    public void setSpeed(double speed) {
+        intakeMotor.set(speed);
+    }
 }
+
--- src/main/java/frc/robot/Pivot.java
+++ src/main/java/frc/robot/Pivot.java
@@ -7,7 +7,6 @@ import frc.team5431.titan.core.joysticks.LogitechExtreme3D;
 
 public class Pivot {
     public class Drivebase extends TimedRobot {
-        private LogitechExtreme3D driver;
         private LogitechExtreme3D operator;
         Math power;
 
