68781f8b353e2722b6906fda8578af9559c1fc4c
aahana <86615215+aahana007@users.noreply.github.com>
2022-04-12 13:19:25 -0500

    Pivot skel

--- src/main/java/frc/robot/Pivot.java
+++ src/main/java/frc/robot/Pivot.java
@@ -1,14 +1,35 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.TimedRobot;
-import frc.team5431.titan.core.joysticks.LogitechExtreme3D;
+import frc.team5431.titan.core.joysticks.LogitechExtreme3D; 
 
+/**
+ * @author Aahana Shrivastava
+ */
 public class Pivot {
-    public class Drivebase extends TimedRobot {
-        private LogitechExtreme3D operator;
-        Math power;
+        private CANSparkMax pivotMotor;
 
-        private final CANSparkMax pivotMotor = new CANSparkMax(5, MotorType.kBrushless);
+    public static final double PIVOT_DOWN_LIMIT = -42000; // -45k // -49k
+    public static final double PIVOT_UP_LIMIT = 0;
+
+public Pivot(CANSparkMax pivotMotor) {
+    this.pivotMotor = pivotMotor;
+    this.pivotMotor.enableSoftLimit(SoftLimitDirection.kForward, true);
+    this.pivotMotor.enableSoftLimit(SoftLimitDirection.kReverse, true);
+    this.pivotMotor.setSoftLimit(SoftLimitDirection.kForward, 100);
+    this.pivotMotor.setSoftLimit(SoftLimitDirection.kReverse, 100);
+
+}
+
+public void calibrateMode(boolean value) {
+    this.pivotMotor.enableSoftLimit(SoftLimitDirection.kForward, !value);
+    this.pivotMotor.enableSoftLimit(SoftLimitDirection.kReverse, !value);
+}
+
+public void reset() {
+    this.pivotMotor.getEncoder().setPosition(0);
+}
+
 }
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+import frc.team5431.titan.core.joysticks.LogitechExtreme3D; 
+import frc.team5431.titan.core.joysticks.Xbox;
+public class RobotContainer {
+    
+}
