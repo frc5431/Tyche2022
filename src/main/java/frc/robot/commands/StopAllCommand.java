7e2f8a962a99fcc67117122ceeda86a14ff7a713
aahana <86615215+aahana007@users.noreply.github.com>
2022-04-20 10:42:11 -0500

    Climber Extend

---
+++ src/main/java/frc/robot/commands/StopAllCommand.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants;
+import frc.robot.Systems;
+import frc.robot.commands.subsystems.*;
+import frc.robot.subsystems.Shooter;
+
+
+public class StopAllCommand extends ParallelCommandGroup {
+    public StopAllCommand(Systems systems) {
+        addCommands(
+            new IntakeCommand(systems, 0),
+            new FeederBottomCommand(systems, 0),
+            new FeederTopCommand(systems, 0),
+            new ShooterCommand(systems, Shooter.VELOCITY_NORMAL),
+            new DriveCommand(systems, 0, 0, 0),
+            new ClimberExtendCommand(systems, 0),
+            new ClimberHingeCommand(systems, 0),
+            new PivotCommand(systems, 0)
+        );
+    }
+}
---
+++ src/main/java/frc/robot/commands/subsystems/ClimberExtendCommand.java
@@ -0,0 +1,64 @@
+package frc.robot.commands.subsystems;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Systems;
+import frc.robot.subsystems.Climber;
+import frc.team5431.titan.core.misc.Logger;
+
+public class ClimberExtendCommand extends CommandBase {
+    private final Climber climber;
+    private final boolean reverse;
+	private final DoubleSupplier supplier;
+
+    public ClimberExtendCommand(Systems systems, boolean reverse) {
+        this(systems, () -> Climber.DEFAULT_SPEED_EXTEND, reverse);
+    }
+
+    public ClimberExtendCommand(Systems systems, double speed) {
+        this(systems, () -> speed, false);
+    }
+
+    public ClimberExtendCommand(Systems systems, DoubleSupplier supplier) {
+        this(systems, supplier, false);
+    }
+
+    public ClimberExtendCommand(Systems systems, DoubleSupplier supplier, boolean reverse) {
+        this.climber = systems.getClimber();
+        this.reverse = reverse;
+		this.supplier = supplier;
+
+        addRequirements(climber.getExtend());
+    }
+
+    public ClimberExtendCommand(Systems systems, Object object, boolean reverse2) {
+    }
+
+    public ClimberExtendCommand(Systems systems, Object object, boolean reverse2) {
+    }
+
+    public ClimberExtendCommand(Systems systems, Object object, boolean reverse2) {
+    }
+
+    @Override
+    public void initialize() {
+
+	}
+	
+	@Override
+	public void execute() {
+        double value = supplier.getAsDouble();
+        climber.setExtend(reverse ? -value : value);
+	}
+
+    @Override
+    public void end(boolean interrupted) {
+        Logger.l("Climber Extend Command Done");
+        climber.setExtend(0);
+    }
+    
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
