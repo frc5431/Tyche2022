1f24180f8aef3102790f7e28c7fc16372449c2a1
aahana <86615215+aahana007@users.noreply.github.com>
2022-05-09 15:20:13 -0500

    Pivot motors

--- src/main/java/frc/robot/Subsystems/Pivot.java
+++ src/main/java/frc/robot/Subsystems/Pivot.java
@@ -1,12 +1,11 @@
 package frc.robot.subsystems;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team5431.titan.core.joysticks.LogitechExtreme3D; 
-
-import static java.lang.Math.max;
-import static java.lang.Math.min;
 /**
  * @author Aahana Shrivastava
  */
@@ -21,7 +20,6 @@ public Pivot(VictorSPX pivotMotor) {
     this.pivotMotor.configForwardSoftLimitThreshold(max(PIVOT_UP_LIMIT, PIVOT_DOWN_LIMIT));
     this.pivotMotor.configReverseSoftLimitEnable(true);
     this.pivotMotor.configReverseSoftLimitThreshold(min(PIVOT_UP_LIMIT, PIVOT_DOWN_LIMIT));
-
 }
 
 public void calibrateMode(boolean value) {
